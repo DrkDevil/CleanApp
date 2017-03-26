d6c258334ec1bf25f26a3fb007f21e861be5cc14
Brian Orr <brian@digitalreligion.net>
2017-03-25 21:00:07 -0500

    Minor Edits
    moved some things around.  See Git Versioning.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,5 @@
 angular.module('BonsaiApp',[
   'BonsaiApp.index',
   'BonsaiApp.newView',
-  'templates',
-  'ngRoute'	
+  'templates'
 ]);
--- app/scripts/index/index.js
+++ app/scripts/index/index.js
@@ -1,5 +1,6 @@
 'use strict';
 angular.module('BonsaiApp.index', ['ngRoute','index.controllers','index.directives'])
+
 .run(['$rootScope', '$route', '$location', function($rootScope, $route, $location) {
   $rootScope.$on('$routeChangeSuccess', function(newVal, oldVal) {
     if (oldVal !== newVal) {
@@ -7,11 +8,12 @@ angular.module('BonsaiApp.index', ['ngRoute','index.controllers','index.directiv
     }
   });
 }])
+
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', { title: 'BonsaiApp - BaseApp with Modern UI', templateUrl: 'main.html', controller: 'IndexCtrl' })
-    .when('/about', { title: 'BonsaiApp - About', templateUrl: 'about.html', controller: 'IndexCtrl' })
-	.when('/soon', { title: 'Under Construction', templateUrl: 'soon.html', controller: 'IndexCtrl' })
+    .when('/about', { title: 'BonsaiApp - About BonsaiApp', templateUrl: 'about.html', controller: 'IndexCtrl' })
+  	.when('/soon', { title: 'BonsaiApp - Coming Soon', templateUrl: 'soon.html', controller: 'IndexCtrl' })
     .otherwise({ redirectTo: '/' });
   $locationProvider.html5Mode(true);
 }]);
