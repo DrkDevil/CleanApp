fbfb28064ca963dc4307798f342da274f1a213b6
Brian Orr <brian@digitalreligion.net>
2017-03-27 17:47:42 -0500

    Major Update
    Added Splash Page, and rearanged several items, renamed, and removed uneeded files for clean version. Added logo artwork, and backgrounds. Added some demo css.

--- app/scripts/newView/index.js
+++ app/scripts/newView/index.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('BonsaiApp.newView', ['ngRoute','index.controllers','index.directives'])
+angular.module('BonsaiApp.newView', ['ngRoute','newView.controllers','index.directives'])
 .run(['$rootScope', '$route', '$location', function($rootScope, $route, $location) {
   $rootScope.$on('$routeChangeSuccess', function(newVal, oldVal) {
     if (oldVal !== newVal) {
@@ -10,6 +10,7 @@ angular.module('BonsaiApp.newView', ['ngRoute','index.controllers','index.direct
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/newView', { title: 'BonsaiApp - BaseApp with Modern UI', templateUrl: 'newView.html', controller: 'newViewCtrl' })
+    .when('/about', { title: 'BonsaiApp - About BonsaiApp', templateUrl: 'about.html', controller: 'newViewCtrl' })
     .otherwise({ redirectTo: '/' });
   $locationProvider.html5Mode(true);
 }]);
