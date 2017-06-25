3aaaa576f9ecdd4e96787670fbdaccf53518dcef
Brian Orr <brian@digitalreligion.net>
2017-06-24 21:37:30 -0500

    Dropdown edits
    removed onclick funtcion from HTML. In dropdown.js I took the function out of the loop by creating a callback.

--- app/scripts/bonsai/dropDowns.js
+++ app/scripts/bonsai/dropDowns.js
@@ -9,31 +9,33 @@ Licensed under MIT
 When the user clicks on the button, toggle between hiding and showing the dropdown content 
 ============================================================================================
  */
-
-function dropDown() {
-    'use strict';
-	// Get the Value of data-dropdown 
-	var b = document.getElementsByTagName('button')[0].getAttribute('data-dropdown');
-	console.log(b);	
-	// Get the value of dropdown-content's ID 
-	var c = document.querySelector('.dropdown-content');
-    console.log(c);	
-	
-	
-	// This is here cause it shows the drop down on the demo
-	document.getElementById("myDropDown").classList.toggle("show");		
+var buttons = document.getElementsByTagName("button");
+function buttonCallBack() {
+	"use strict";
+	var a = this.getAttribute('data-dropdown');
+	var b = document.getElementsByClassName('dropdown-content')[0].id;
+	console.log(a);
+	console.log(b);
+	if (a === b) {
+		document.getElementById(b).classList.toggle("show");
+	} else {
+		alert("not equal");
+	}
+}
+for (var i = 0; i <= buttons.length; i += 1) {
+	buttons[i].onclick = buttonCallBack;
 }
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  "use strict";
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-     for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+window.onclick = function (event) {
+	"use strict";
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
 };
