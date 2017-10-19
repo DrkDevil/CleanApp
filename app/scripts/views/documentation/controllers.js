51d9f07c4e4c0d903225d21a2a6259a9dbd22459
Brian Orr <brian@digitalreligion.net>
2017-10-18 21:07:36 -0500

    Created documents section
    and moved templates.js back cause it caused issues

--- app/scripts/views/documentation/controllers.js
+++ app/scripts/views/documentation/controllers.js
@@ -7,6 +7,22 @@ angular.module('docs.controllers', [])
   $('html').removeClass().addClass('docs');	
 }])
 
+.controller('introCtrl', ['$scope', '$location', function($scope, $location) {
+  $scope.$on('$locationChangeStart', function(scope) {
+    $scope.pathUrl = $location.path();($scope.pathUrl);
+  });
+  // Add Page Class for more control over page specific classes
+  $('html').removeClass().addClass('intro');	
+}])
+
+.controller('startedCtrl', ['$scope', '$location', function($scope, $location) {
+  $scope.$on('$locationChangeStart', function(scope) {
+    $scope.pathUrl = $location.path();($scope.pathUrl);
+  });
+  // Add Page Class for more control over page specific classes
+  $('html').removeClass().addClass('started');	
+}])
+
 .controller('soonCtrl', ['$scope', '$location', function($scope, $location) {
   $scope.$on('$locationChangeStart', function(scope) {
     $scope.pathUrl = $location.path();($scope.pathUrl);
--- app/scripts/views/documentation/index.js
+++ app/scripts/views/documentation/index.js
@@ -9,7 +9,8 @@ angular.module('BonsaiApp.docs', ['ngRoute','docs.controllers','docs.directives'
 }])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
-    .when('/docs', { title: 'BonsaiApp - BaseApp with Modern UI', templateUrl: 'docs.html', controller: 'docsCtrl' })
+    .when('/docs', { title: 'BonsaiApp - Documentation', templateUrl: 'docs.html', controller: 'docsCtrl' })
+    .when('/started', { title: 'BonsaiApp - Getting Started', templateUrl: 'started.html', controller: 'docsCtrl' })
     .otherwise({ redirectTo: '/' });
   $locationProvider.html5Mode(true);
 }]);
