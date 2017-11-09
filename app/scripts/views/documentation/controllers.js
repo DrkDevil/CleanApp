21364319bdf0ca872430a32c3fc5386ff8b706f8
Brian Orr <brian@digitalreligion.net>
2017-11-08 19:55:58 -0600

    Typography  Edits
    Typography change. fontsizes and button scss finalization

--- app/scripts/views/documentation/controllers.js
+++ app/scripts/views/documentation/controllers.js
@@ -5,4 +5,11 @@ angular.module('docs.controllers', [])
   });
   // Add Page Class for more control over page specific classes
   $('html').removeClass().addClass('docs');	
+}])
+.controller('alertsCtrl', ['$scope', '$location', function($scope, $location) {
+  $scope.$on('$locationChangeStart', function(scope) {
+    $scope.pathUrl = $location.path();($scope.pathUrl);
+  });
+  // Add Page Class for more control over page specific classes
+  $('html').removeClass().addClass('alrt');	
 }]);
--- app/scripts/views/documentation/index.js
+++ app/scripts/views/documentation/index.js
@@ -12,6 +12,7 @@ angular.module('BonsaiApp.docs', ['ngRoute','docs.controllers','docs.directives'
     .when('/docs', { title: 'BonsaiApp - Documentation', templateUrl: 'docs.html', controller: 'docsCtrl' })
 	.when('/ui', { title: 'BonsaiApp - BonsaiUI', templateUrl: 'ui.html', controller: 'docsCtrl' })
 	.when('/alerts', { title: 'BonsaiApp - Alerts and Notifications', templateUrl: 'alerts.html', controller: 'alertsCtrl' })
+	.when('/badges', { title: 'BonsaiApp - Badges', templateUrl: 'badges.html', controller: 'badgesCtrl' })
     .when('/started', { title: 'BonsaiApp - Getting Started', templateUrl: 'started.html', controller: 'docsCtrl' })
     .otherwise({ redirectTo: '/' });
   $locationProvider.html5Mode(true);
