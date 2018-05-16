7282e217581ecf5d756a48a383d066388a8ab89d
Brian Orr <brian@digitalreligion.net>
2018-05-16 09:24:59 -0500

    Moved allDocumentation in to the Docs folder, added an includes folder and added the sidebar include with links to all the new pages that were created as well. .

--- app/scripts/views/documentation/controllers.js
+++ app/scripts/views/documentation/controllers.js
@@ -4,12 +4,5 @@ angular.module('docs.controllers', [])
     $scope.pathUrl = $location.path();($scope.pathUrl);
   });
   // Add Page Class for more control over page specific classes
-  $('html').removeClass().addClass('docs');	
-}])
-.controller('alertsCtrl', ['$scope', '$location', function($scope, $location) {
-  $scope.$on('$locationChangeStart', function(scope) {
-    $scope.pathUrl = $location.path();($scope.pathUrl);
-  });
-  // Add Page Class for more control over page specific classes
-  $('html').removeClass().addClass('alrt');	
+  $('html').removeClass().addClass('docs');
 }]);
