13662fabad5d9d24c3a972e6e22715b850389ab7
Brian Orr <brian@digitalreligion.net>
2018-07-07 14:48:28 -0500

    Added repl loop to gulp file so you can lint at anytime durring development

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
-// ----- BonsaiApp - Clean Version - Gulp File ------------------------------------------------                                           
+// ----- BonsaiApp - Clean Version - Gulp File ------------------------------------------------
 // ----- Required Dependecies -----------------------------------------------------------------
 var gulp              = require('gulp');
 var sass              = require('gulp-sass');
@@ -7,14 +7,28 @@ var eslint            = require('gulp-eslint');
 var templateCache     = require('gulp-angular-templatecache');
 var friendlyFormatter = require('eslint-friendly-formatter');
 var browserSync       = require('browser-sync');
+var repl              = require('gulp-repl');
 
 // ----- Reload BrowserSync -------------------------------------------------------------------
 var reload = browserSync.reload;
+gulp.task('BonsaiApp', function (cb) {
+  gulp.repl = repl.start(gulp);
+});
+gulp.task('stop', function (cb) {
+  if (gulp.repl) {
+    gulp.repl.close(); // same as nodejs.org/api/readline.html#readline_rl_close
+  }
+  cb();
+});
 
-// ---- Gulp Quick Commands -------------------------------------------------------------------
-gulp.task('prod', ['lint', 'build', 'view']);      // gulp prod
-gulp.task('dev', ['lint', 'view']);                // gulp dev
-gulp.task('default', ['dev']);                     // gulp default
+// ---- JavaScript Linting (ESlint) -----------------------------------------------------------
+gulp.task('lint', function(cb) {
+  return gulp.src(['app/scripts/**/*.js',          // Location of js files you want to lint.
+                   '!app/scripts/templates.js'])   // Use !foldername/filename to ignore files.
+    .pipe(eslint('.eslintrc.json'))                // Your eslint pipe (config file)
+    .pipe(eslint.format(friendlyFormatter));       // A better way to dispay errors.
+  cb();
+});
 
 // ---- Sass Compiling ------------------------------------------------------------------------
 gulp.task('sass', function() {
@@ -30,31 +44,28 @@ gulp.task('templates', function() {
    .pipe(gulp.dest('app/scripts'));          // Create template.js in designated folder.
 });
 
-// ---- JavaScript Linting (ESlint) -----------------------------------------------------------
-gulp.task('lint', function() {
-  return gulp.src(['app/scripts/**/*.js',          // Location of js files you want to lint. 
-				   '!app/scripts/templates.js'])   // Use !foldername/filename to ignore files. 
-    .pipe(eslint('.eslintrc.json'))                // Your eslint pipe (config file) 
-    .pipe(eslint.format(friendlyFormatter));       // A better way to dispay errors. 
-});
-
 // ---- BrowserSync Config --------------------------------------------------------------------
-gulp.task('view', [], function() {
+gulp.task('view', [], function(cb) {
   browserSync({
     notify: false,            // Enable or Disables the 'BrowserSync is running' notification
-    logPrefix: 'BonsaiApp',   // Name you want displayed in your console log's prefix. 
+    logPrefix: 'BonsaiApp',   // Name you want displayed in your console log's prefix.
     server: ['./app'],
 	// The folder the application is running from.
     // https: true;
 	// Run as an https by uncommenting 'https: true'
 	// Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.   
+    //       will present a certificate warning in the browser.
   });
-  // -- Watch these files, and folders. 
-  // -- If there are any changes to saved files reload browser. 
+  // -- Watch these files, and folders.
+  // -- If there are any changes to saved files reload browser.
   gulp.watch('app/index.html').on('change', reload);
   gulp.watch('app/templates/**/*.html', ['templates', reload]);
   gulp.watch('app/scripts/**/*.js').on('change', reload);
   gulp.watch('app/styles/**/*.css').on('change', reload);
   gulp.watch('app/scss/**/*.scss', ['sass', reload]);
+  cb();
 });
+
+// ---- Gulp Quick Commands -------------------------------------------------------------------
+gulp.task('default', ['BonsaiApp', 'lint', 'view']);              // gulp default
+gulp.task('dev', ['lint', 'view']);               // gulp dev
